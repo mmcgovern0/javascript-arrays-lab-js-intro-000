67a3ad95777db64e13363ab07a54251f590d3fd4
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-22 20:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
 }
 
-function destrivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
 
